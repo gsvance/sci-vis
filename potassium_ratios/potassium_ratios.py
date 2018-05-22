fb02ae91654e2e088f4dd6db523a4bf1561d95f7
Greg Vance <gsvance@asu.edu>
2018-05-22 10:15:48 -0700

    Updated potassium code to include total K abundance with everything else

--- potassium_ratios/potassium_ratios.py
+++ potassium_ratios/potassium_ratios.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 
 # For each sim, report the min and max per-particle values of the following ratios and abundances:
-#     40K/39K, 40K/K, K/Ca, Ca, Si, K/Si, K/Mg
+#     40K/39K, 40K/K, K/Ca, Ca, Si, K/Si, K/Mg, K
 # Also produce text files with complete lists of these values for histograms and scatter plots
 
-# Last modified 11/22/16 by Greg Vance
+# Last modified 5/22/18 by Greg Vance
 
 import os
 
 # The directory containing all of the simulation data
-DATA_DIR = "/home/gsvance/sn_data/"
+DATA_DIR = "/home/gsvance/supernova_data/"
 # Tuple of the relevant isotopes for these calculations
-ISOTOPES = ("24Mg", "25Mg", "26Mg", "39K", "40K", "40Ca", "41Ca", "44Ca", "28Si", "29Si", "30Si")
+ISOTOPES = ("24Mg", "25Mg", "26Mg", "39K", "40K", "41K",
+	"40Ca", "41Ca", "42Ca", "43Ca", "44Ca", "28Si", "29Si", "30Si")
 # Calculated values that should be added in
-CALCULATED = (("K", lambda x: x["39K"] + x["40K"]),
-	("Ca", lambda x: x["40Ca"] + x["41Ca"] + x["44Ca"]),
-	("Si", lambda x: x["28Si"] + x["29Si"] + x["30Si"]),
-	("Mg", lambda x: x["24Mg"] + x["25Mg"] + x["26Mg"]))
+CALCULATED = (("K", lambda x : x["39K"] + x["40K"] + x["41K"]),
+	("Ca", lambda x : x["40Ca"] + x["41Ca"] + x["42Ca"] + x["43Ca"] + x["44Ca"]),
+	("Si", lambda x : x["28Si"] + x["29Si"] + x["30Si"]),
+	("Mg", lambda x : x["24Mg"] + x["25Mg"] + x["26Mg"]))
 # Ratios and abundances that we are interested in knowing about
-RATIOS = (("40K/39K", lambda x: x["40K"] / x["39K"]),
-	("40K/K", lambda x: x["40K"] / x["K"]),
-	("K/Ca", lambda x: x["K"] / x["Ca"]),
-	("40K/Ca", lambda x: x["40K"] / x["Ca"]),
-	("Ca", lambda x: x["Ca"]),
-	("Si", lambda x: x["Si"]),
-	("K/Si", lambda x: x["K"] / x["Si"]),
-	("K/Mg", lambda x: x["K"] / x["Mg"]))
+RATIOS = (("40K/39K", lambda x : x["40K"] / x["39K"]),
+	("40K/K", lambda x : x["40K"] / x["K"]),
+	("K/Ca", lambda x : x["K"] / x["Ca"]),
+	("40K/Ca", lambda x : x["40K"] / x["Ca"]),
+	("Ca", lambda x : x["Ca"]),
+	("Si", lambda x : x["Si"]),
+	("K/Si", lambda x : x["K"] / x["Si"]),
+	("K/Mg", lambda x : x["K"] / x["Mg"]),
+	("K", lambda x : x["K"]))
 
 def main():
 	# Loop over all the simulations and carry out the appropriate calculations for each
@@ -34,7 +36,7 @@ def main():
 		print "%s:" % (sim)
 		# Read in and organize all of the available data for this simulation
 		data = get_data(sim)
-		# Add in the calculated total values for both K and Ca
+		# Add in the calculated total values for all the elements we need
 		data = add_calculated(data)
 		# Make dictionaries of every obtainable value for the desired ratios
 		ratios = get_ratios(data)
