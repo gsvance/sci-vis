f6af0d8ab44a0740da4c06816b5c66955dccd4ca
Greg Vance <gsvance@asu.edu>
2018-09-21 13:31:04 -0700

    Wrote header of plot_lines.py

---
+++ thermal_trajectories/plot_lines.py
@@ -0,0 +1,13 @@
+#!/ubr/bin/env python
+
+# Read the fetched data files and do the actual plotting work
+# Make spaghetti plots of the yellow region particle trajectories
+# Also produce cleaner plots with the visual spread statistics
+
+# Last modified 9/21/18 by Greg Vance
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
