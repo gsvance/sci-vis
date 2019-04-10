6ad19dbf26aa4b78d466a01c26ea9d6028e02904
Greg Vance <gsvance@asu.edu>
2019-04-09 23:01:24 -0700

    Updated entropy regions list code to no longer use entropy regions

--- evolution_plots/entropy_regions/make_lists.py
+++ evolution_plots/entropy_regions/make_lists.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-# The cco2 histogram of Fe/Ti vs radiation entropy displays 4 distinct regions:
-#  - Region 1: bright yellow blob appearing at log(S_rad) ~ 22.5
-#  - Region 2: smeared patch of reddish bins up at log(Fe/Ti) ~ 3.5
-#  - Region 3: crescent shape with bright yellow bins at log(S_rad) ~ 22.6
-#  - Region 4: extended region of high-entropy particles at log(S_rad) ~ 23.1
+# The cco2 magkotsios plot of the Fe/Ti ratio shows 4 distinct regions:
+#  - Region 1: blue-reddish blob in bottom left at log(rho) ~ 6.1
+#  - Region 2: bright fat yellow-orange blob at log(rho) ~ 6.4
+#  - Region 3: extended blue-ish region spanning temp ~ 6 to 8
+#  - Region 4: isolated linear blue region out at temp ~ 9.5
 # Save a list of particle IDs for everything within these 4 regions
 # Save a list of all the cco2 SDF files in order with tpos values
 # These lists will be read by fetch_data.c when it does its job next
 # Also write a file listing particle IDs by region for the plots later
+## The regions were *previously* defined using the Fe/Ti vs S_rad histogram:
+##  - Region 1: bright yellow blob appearing at log(S_rad) ~ 22.5
+##  - Region 2: smeared patch of reddish bins up at log(Fe/Ti) ~ 3.5
+##  - Region 3: crescent shape with bright yellow bins at log(S_rad) ~ 22.6
+##  - Region 4: extended region of high-entropy particles at log(S_rad) ~ 23.1
+## Turns out that in that scheme, regions 1 thru 3 are not well-separated
 
-# Last modified 4/5/19 by Greg Vance
+# Last modified 4/9/19 by Greg Vance
 
 import numpy as np
 import os
@@ -29,23 +35,42 @@ COLS_FILE = PLOT_DIR + "columns"
 SDF_DIR = "/home/gsvance/supernova_data/cco2/cco2/"
 
 # Approximate boundaries of the regions from an interactive MPL session
-# See young/shared_files/casA_plots/hist_fe_ti_vs_rad_ent.png
-REG1_SRAD_MIN = 10.**22.47 # K^3 cm^3 g^-1
-REG1_SRAD_MAX = 10.**22.55 # K^3 cm^3 g^-1
-REG1_FETI_MIN = 10.**2.4
-REG1_FETI_MAX = 10.**2.7
-REG2_SRAD_MIN = 10.**22.55 # K^3 cm^3 g^-1
-REG2_SRAD_MAX = 10.**22.63 # K^3 cm^3 g^-1
-REG2_FETI_MIN = 10.**3.5
-REG2_FETI_MAX = 10.**4.1
-REG3_SRAD_MIN = 10.**22.56 # K^3 cm^3 g^-1
-REG3_SRAD_MAX = 10.**22.70 # K^3 cm^3 g^-1
-REG3_FETI_MIN = 10.**2.2
-REG3_FETI_MAX = 10.**2.8
-REG4_SRAD_MIN = 10.**23.02 # K^3 cm^3 g^-1
-REG4_SRAD_MAX = 10.**23.16 # K^3 cm^3 g^-1
-REG4_FETI_MIN = 10.**0.9
-REG4_FETI_MAX = 10.**4.1
+# See young/shared_files/magkotsios/cco2_magkotsios_feti.png
+REG1_PTEMP_MIN = 3.3e9 # K
+REG1_PTEMP_MAX = 4.2e9 # K
+REG1_PDENS_MIN = 10.**5.9 # g cm^-3
+REG1_PDENS_MAX = 10.**6.4 # g cm^-3
+REG2_PTEMP_MIN = 4.2e9 # K
+REG2_PTEMP_MAX = 5.3e9 # K
+REG2_PDENS_MIN = 10.**6.1 # g cm^-3
+REG2_PDENS_MAX = 10.**6.8 # g cm^-3
+REG3_PTEMP_MIN = 5.3e9 # K
+REG3_PTEMP_MAX = 8.4e9 # K
+REG3_PDENS_MIN = 10.**6.6 # g cm^-3
+REG3_PDENS_MAX = 10.**7.3 # g cm^-3
+REG4_PTEMP_MIN = 8.4e9 # K
+REG4_PTEMP_MAX = 10.1e9 # K
+REG4_PDENS_MIN = 10.**6.5 # g cm^-3
+REG4_PDENS_MAX = 10.**7.0 # g cm^-3
+
+## Approximate boundaries of the regions from an interactive MPL session
+## See young/shared_files/casA_plots/hist_fe_ti_vs_rad_ent.png
+#REG1_SRAD_MIN = 10.**22.47 # K^3 cm^3 g^-1
+#REG1_SRAD_MAX = 10.**22.55 # K^3 cm^3 g^-1
+#REG1_FETI_MIN = 10.**2.4
+#REG1_FETI_MAX = 10.**2.7
+#REG2_SRAD_MIN = 10.**22.55 # K^3 cm^3 g^-1
+#REG2_SRAD_MAX = 10.**22.63 # K^3 cm^3 g^-1
+#REG2_FETI_MIN = 10.**3.5
+#REG2_FETI_MAX = 10.**4.1
+#REG3_SRAD_MIN = 10.**22.56 # K^3 cm^3 g^-1
+#REG3_SRAD_MAX = 10.**22.70 # K^3 cm^3 g^-1
+#REG3_FETI_MIN = 10.**2.2
+#REG3_FETI_MAX = 10.**2.8
+#REG4_SRAD_MIN = 10.**23.02 # K^3 cm^3 g^-1
+#REG4_SRAD_MAX = 10.**23.16 # K^3 cm^3 g^-1
+#REG4_FETI_MIN = 10.**0.9
+#REG4_FETI_MAX = 10.**4.1
 
 # Read columns file and extract indicies of relevant columns
 with open(COLS_FILE, "r") as cols_file:
@@ -65,26 +90,26 @@ pid, peak_temp, peak_rho, fe, ni, ti = np.loadtxt(PLOT_FILE, datatype,
 	i_56ni, i_44ti), unpack=True)
 print "cco2 plotting data obtained"
 
-# Calculate the Fe/Ti ratios and radiation entropies to identify regions
-srad = peak_temp**3 / peak_rho
-feti = (fe + ni) / ti
-usable = np.isfinite(feti) & (feti != 0.)
-pid, feti, srad = pid[usable], feti[usable], srad[usable]
+## Calculate the Fe/Ti ratios and radiation entropies to identify regions
+#srad = peak_temp**3 / peak_rho
+#feti = (fe + ni) / ti
+#usable = np.isfinite(feti) & (feti != 0.)
+#pid, feti, srad = pid[usable], feti[usable], srad[usable]
 
 # Make a list of the particle ID for every particle in each region
 reg1_pids, reg2_pids, reg3_pids, reg4_pids = [], [], [], []
 for i in xrange(pid.size):
-	if REG1_SRAD_MIN < srad[i] < REG1_SRAD_MAX \
-		and REG1_FETI_MIN < feti[i] < REG1_FETI_MAX:
+	if REG1_PTEMP_MIN < peak_temp[i] < REG1_PTEMP_MAX \
+		and REG1_PDENS_MIN < peak_rho[i] < REG1_PDENS_MAX:
 		reg1_pids.append(pid[i])
-	elif REG2_SRAD_MIN < srad[i] < REG2_SRAD_MAX \
-		and REG2_FETI_MIN < feti[i] < REG2_FETI_MAX:
+	elif REG2_PTEMP_MIN < peak_temp[i] < REG2_PTEMP_MAX \
+		and REG2_PDENS_MIN < peak_rho[i] < REG2_PDENS_MAX:
 		reg2_pids.append(pid[i])
-	elif REG3_SRAD_MIN < srad[i] < REG3_SRAD_MAX \
-		and REG3_FETI_MIN < feti[i] < REG3_FETI_MAX:
+	elif REG3_PTEMP_MIN < peak_temp[i] < REG3_PTEMP_MAX \
+		and REG3_PDENS_MIN < peak_rho[i] < REG3_PDENS_MAX:
 		reg3_pids.append(pid[i])
-	elif REG4_SRAD_MIN < srad[i] < REG4_SRAD_MAX \
-		and REG4_FETI_MIN < feti[i] < REG4_FETI_MAX:
+	elif REG4_PTEMP_MIN < peak_temp[i] < REG4_PTEMP_MAX \
+		and REG4_PDENS_MIN < peak_rho[i] < REG4_PDENS_MAX:
 		reg4_pids.append(pid[i])
 all_pids = []
 all_pids.extend([(pid1, "R1") for pid1 in reg1_pids])
